f1598c95b25f5c58100d0297d5c7f26a1346d688
avalanchy <avalanchy@gmail.com>
2014-05-31 02:00:59 +0200

    Hodor meets Hodor

--- hodor.py
+++ hodor.py
@@ -4,10 +4,18 @@
 Hodor.
 """
 
+import sys
+sys.tracebacklimit = 0  # python 2.7
+
 __version__ = 'Hodor.'
 __author__ = 'Hodor.'
 __copyright__ = 'Hodor.'
 
+
+class HodorError(RuntimeError):
+    """
+    In case when Hodor meets another Hodor
+    """
 
 # Hodor.
 HODOR = 'Hodor.'
@@ -27,11 +35,20 @@ class Hodor(object):
         hodor = self._hodor
         return hodor
 
+    def hodor_hodor(self):
+        return self.hodor_hodor()
+
     def __call__(self):
         hodor = self.hodor()
         return hodor
+
 hodor = Hodor(HODOR)
 
 
 if __name__ == '__main__':
     print(hodor.hodor())
+
+    try:
+        print(hodor.hodor_hodor())
+    except RuntimeError as e:
+        raise HodorError(e.message.replace('recursion', 'hodor'))
